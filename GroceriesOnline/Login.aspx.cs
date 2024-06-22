6d15d43374ceaf462c0e2fabdde715eb507095fc
dyerex <iffatikhwan@gmail.com>
2024-06-22 17:53:47 +0800

    Update Login.aspx.cs

--- GroceriesOnline/Login.aspx.cs
+++ GroceriesOnline/Login.aspx.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Web.UI;
 
 namespace GroceriesOnline
@@ -11,20 +14,59 @@ namespace GroceriesOnline
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
+
             string username = txtUsername.Text;
             string password = txtPassword.Text;
 
-            // Dummy check for demonstration purposes. Replace with actual authentication logic.
-            if (username == "admin" && password == "password")
+            string sql = @"SELECT * FROM Users WHERE UserName = @username";
+            SqlConnection conn = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["connGrocerShop"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@username", username);
+
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+
+            if (dt.Rows.Count > 0)
             {
-                // Redirect to a different page upon successful login
-                Response.Redirect("HomePage.aspx");
+                //Username found
+                Object objPasswordHash = dt.Rows[0]["PasswordHash"];
+                Object objRole = dt.Rows[0]["Role"];
+                Object objEnabled = dt.Rows[0]["Enabled"];
+                string storedPasswordHash = objPasswordHash.ToString();
+                PBKDF2Hash hash = new PBKDF2Hash(password, storedPasswordHash);
+                bool check = hash.PasswordCheck;
+                bool enabled = Convert.ToBoolean(objEnabled);
+
+                if (check == true && enabled == true)
+                {
+
+                    //password correct and account enabled
+                    Session["UserName"] = username;
+                    Session["Role"] = objRole;
+
+                    if (Session["Role"].ToString() == "user")
+                        Response.Redirect("Shop.aspx");
+                    else if (Session["Role"].ToString() == "admin")
+                        Response.Redirect("Admin.aspx");
+
+
+                }
+                else
+                {
+                    //password incorrect or account disabled
+                    lblStatus.Text = "Incorrect password or account disabled.";
+                }
+
             }
             else
             {
-                Label1.Text = "Invalid username or password";
-                Label1.Visible = true;
+                //Username not found
+                lblStatus.Text = "Incorrect username";
             }
+
+
         }
     }
 }
