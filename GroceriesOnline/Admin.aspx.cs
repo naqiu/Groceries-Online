ebee1f2945d02220e488bf297be753fafa112def
dyerex <iffatikhwan@gmail.com>
2024-06-22 17:52:39 +0800

    Create Admin.aspx.cs

---
+++ GroceriesOnline/Admin.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GroceriesOnline
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Role"] == null || Session["Role"].ToString() != "admin")
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
